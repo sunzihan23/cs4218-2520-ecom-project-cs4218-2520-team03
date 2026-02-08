b261b059a90567df15029262fd236a641afc104c
Zihan <e0970553@u.nus.edu>
2026-02-08 18:56:53 +0800

    add unit tests for Profile component

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,13 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  //testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/user/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  //collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: ["client/src/pages/user/**"],
   coverageThreshold: {
     global: {
       lines: 100,
