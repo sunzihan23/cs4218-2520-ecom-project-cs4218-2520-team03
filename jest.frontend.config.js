334466f827a926d233aaad6b45989e84bdd88784
Zihan <e0970553@u.nus.edu>
2026-02-14 17:52:53 +0800

    add unit tests for AuthProvider and useAuth hook

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -25,6 +25,7 @@ export default {
     "<rootDir>/client/src/components/*.test.js",
     "<rootDir>/client/src/pages/admin/*.test.js",
     "<rootDir>/client/src/pages/user/*.test.js",
+    "<rootDir>/client/src/context/*.test.js",
   ],
 
   // jest code coverage
@@ -38,6 +39,7 @@ export default {
     "client/src/pages/admin/AdminDashboard.js",
     "client/src/pages/user/Profile.js",
     "client/src/components/AdminMenu.js",
+    "<rootDir>/client/src/context/auth.test.js"
   ],
   coverageThreshold: {
     global: {
